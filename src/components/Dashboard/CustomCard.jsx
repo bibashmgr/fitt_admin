113777726479d06945d0aa3dfa0ac1c67b855763
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-07-13 20:10:32 +0545

    Add: card in dashboard-page

---
+++ src/components/Dashboard/CustomCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  Flex,
+  Icon,
+  Stat,
+  StatLabel,
+  StatNumber,
+  Grid,
+} from '@chakra-ui/react';
+
+const CustomCard = ({ cardIcon, cardTitle, cardNumber }) => {
+  return (
+    <Grid
+      boxShadow='0px 1px 3px rgba(0, 0, 0, 0.1)'
+      borderRadius={5}
+      px={6}
+      py={6}
+      templateColumns={{ base: 'repeat(2, 1fr)' }}
+    >
+      <Flex justify={{ base: 'start', sm: 'center' }} align='center'>
+        <Icon as={cardIcon} boxSize='35px' />
+      </Flex>
+      <Stat>
+        <StatLabel>{cardTitle}</StatLabel>
+        <StatNumber>
+          {cardNumber < 10 ? '0' + cardNumber : cardNumber}
+        </StatNumber>
+      </Stat>
+    </Grid>
+  );
+};
+
+export default CustomCard;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import { VStack, Grid } from '@chakra-ui/react';
 
+// layouts:
 import MainLayout from '../layouts/MainLayout';
+
+// components:
+import CustomCard from '../components/Dashboard/CustomCard';
+
+// icons
+import { MdFitnessCenter } from 'react-icons/md';
+import { HiOutlineClipboardList } from 'react-icons/hi';
+import { FaUsers, FaUsersCog } from 'react-icons/fa';
 
 const Dashboard = () => {
   return (
     <MainLayout title='Dashboard' pageIndex={1}>
-      <h1>This is Dashboard</h1>
+      <VStack spacing={5} w='100%' align='start'>
+        <Grid
+          templateColumns={{ base: 'repeat(2, 1fr)', lg: 'repeat(4, 1fr)' }}
+          w='100%'
+          gap={4}
+        >
+          <CustomCard cardIcon={FaUsersCog} cardTitle='Admin' cardNumber={3} />
+          <CustomCard cardIcon={FaUsers} cardTitle='Users' cardNumber={10} />
+          <CustomCard
+            cardIcon={MdFitnessCenter}
+            cardTitle='Workouts'
+            cardNumber={120}
+          />
+          <CustomCard
+            cardIcon={HiOutlineClipboardList}
+            cardTitle='Plans'
+            cardNumber={3}
+          />
+        </Grid>
+      </VStack>
     </MainLayout>
   );
 };
